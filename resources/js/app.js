81d108380a9c1bf01da8d8f8963c903a628901e4
ThanapawnTony <113745605+ThanapawnTony@users.noreply.github.com>
2022-11-30 16:26:02 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from "./app.vue";
+
+require('./bootstrap');
+
+const app = createApp(App);
+app.mount("#app-vue");
---
+++ resources/js/app.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Hello Laravel + Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos5App',
+
+    data() {
+        return {
+
+        };
+    },
+    mounted() {
+
+    },
+    method: {
+        
+    },
+    computed(){
+
+    },
+    watch:{
+
+    },
+};
+    
+</script>
+
+<style lang="scss" scoped></style>
+
